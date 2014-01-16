f088e7ec07b93197ef136641a497d38dccc5c09e
rr222cy <rr222cy@student.lnu.se>
2014-01-16 18:32:56 +0100

    Struktur för klassen Reciept

--- CounterReceipt/CounterReceipt/Models/Receipt.cs
+++ CounterReceipt/CounterReceipt/Models/Receipt.cs
@@ -7,5 +7,30 @@ namespace CounterReceipt.Models
 {
     public class Receipt
     {
+        // Fält för klassen.
+        private double _subtotal;
+
+        // Egenskaper för klassen.
+        public double Subtotal
+        {
+            get { return _subtotal; }
+            set { _subtotal = value; }
+        }
+
+        public double DiscountRate { get; private set; }
+        public double MoneyOff { get; private set; }
+        public double Total { get; private set; }
+
+        // Metoder för klassen.
+        public void Calculate(double subtotal)
+        {
+
+        }
+
+        // Konstruktorer för klassen.
+        public Receipt(double subtotal)
+        {
+
+        }
     }
 }
